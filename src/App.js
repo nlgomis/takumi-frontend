42a5b0c4cc00c90014479331af75504bb50b5212
TK4464 <4464tk64@gmail.com>
2024-06-21 17:09:01 +0900

    tailwind router 追加

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Top from "./components/Top";
+import Contact from "./components/Contact";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Top />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
