148c7095a1ee44980110f0d09978e2aa424b8df2
TK4464 <4464tk64@gmail.com>
2024-06-28 17:09:13 +0900

    ディレクトリ構成変更

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import MainPage from "./components/MainPage/MainPage";
 import MyPage from "./components/MyPage/MyPage";
 
@@ -8,10 +10,12 @@ function App() {
     return (
         <Router>
             <div className="App">
+                <Header />
                 <Routes>
                     <Route path="/" element={<MainPage />} />
                     <Route path="/mypage" element={<MyPage />} />
                 </Routes>
+                <Footer />
             </div>
         </Router>
     );
