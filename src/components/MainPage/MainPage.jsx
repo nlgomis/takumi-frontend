1a6ad2d3251320cbc4ce3f1469fba7d98bb1ff2c
TK4464 <4464tk64@gmail.com>
2024-10-08 13:32:50 +0900

    next.jsに変更

--- src/components/MainPage/MainPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MainPage = () => {
-    return <div>top</div>;
-};
-
-export default MainPage;
--- src/components/MyPage/MyPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MyPage = () => {
-    return <div className="text-red-800">MyPage</div>;
-};
-
-export default MyPage;
