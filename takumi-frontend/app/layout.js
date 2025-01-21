3e55778e7dbec3bb788818e003bc866a6f481f03
TK4464 <4464tk64@gmail.com>
2025-01-21 15:54:25 +0900

    フォント変更

--- takumi-frontend/app/layout.js
+++ takumi-frontend/app/layout.js
@@ -1,17 +1,26 @@
 import localFont from "next/font/local";
+import { Shippori_Mincho } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
-const geistSans = localFont({
-    src: "./fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
-});
-const geistMono = localFont({
-    src: "./fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
+// const geistSans = localFont({
+//     src: "./fonts/GeistVF.woff",
+//     variable: "--font-geist-sans",
+//     weight: "100 900",
+// });
+// const geistMono = localFont({
+//     src: "./fonts/GeistMonoVF.woff",
+//     variable: "--font-geist-mono",
+//     weight: "100 900",
+// });
+
+const shipporiMincho = Shippori_Mincho({
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700"],
+    display: "swap",
+    variable: "--font-shippori",
+    preload: true,
 });
 
 export const metadata = {
@@ -21,10 +30,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="ja" className="h-full overflow-x-hidden">
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen bg-black`}
-            >
+        <html
+            lang="ja"
+            className={`${shipporiMincho.variable} h-full overflow-x-hidden `}
+        >
+            <body className="antialiased font-shippori tracking-wider flex flex-col min-h-screen bg-black">
                 <Header />
                 <main className="flex-grow">{children}</main>
                 <Footer />
--- takumi-frontend/tailwind.config.js
+++ takumi-frontend/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        shippori: ["Shippori Mincho", "serif"],
+    },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
