96441c2904d3a8e2aabd322727d42ea027028fad
TK4464 <4464tk64@gmail.com>
2025-02-06 14:53:51 +0900

    レイアウト修正

--- takumi-frontend/components/AccessHome.jsx
+++ takumi-frontend/components/AccessHome.jsx
@@ -27,7 +27,7 @@ const AccessHome = () => {
     return (
         <section className="bg-white py-14">
             <div className="max-w-full mx-auto">
-                <div className="ml-5 lg:ml-12 mb-8">
+                <div className="ml-5 lg:ml-12 mb-8 lg:mb-12">
                     <h2 className="text-darkbrown text-base lg:text-xl font-light tracking-wide mb-1">
                         Access
                     </h2>
@@ -40,7 +40,7 @@ const AccessHome = () => {
                     </div>
                 </div>
 
-                <div className="text-darkbrown text-base md:text-base lg:text-lg px-6 lg:px-28 lg:py-12 ">
+                <div className="text-darkbrown text-base md:text-base lg:text-lg px-6 lg:px-28 lg:pb-12 ">
                     <div className="flex flex-col gap-y-1 lg:gap-y-4">
                         <p>住所：〒160-0023　東京都新宿区西新宿1-7-3</p>
                         <p>電話：03-3344-1010</p>
