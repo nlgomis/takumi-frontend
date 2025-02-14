f98361885f7ecc1acfdd74bb7dba50692fd064ef
TK4464 <4464tk64@gmail.com>
2025-02-14 15:44:20 +0900

    バックエンドと接続

--- takumi-frontend/next.config.mjs
+++ takumi-frontend/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ["tgutgu.s3.ap-northeast-1.amazonaws.com"],
+    },
+};
 
 export default nextConfig;
